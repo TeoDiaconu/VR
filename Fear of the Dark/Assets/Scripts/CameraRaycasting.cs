3c410d6e67e94f60d15260f4eabb185f8934e889
TeoDiaconu <TeoDiaconu@address.com>
2021-01-17 22:58:58 +0200

    scripts

---
+++ Fear of the Dark/Assets/Scripts/CameraRaycasting.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycasting : MonoBehaviour
+{
+    public float range;
+    private IInteractable currentTarget;
+    public Camera mainCamera;
+
+    private void RaycastForInteractable(){
+        RaycastHit hit;
+
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+        
+        if(Physics.Raycast(ray, out hit, range))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+            if(interactable != null)
+            {
+                if(hit.distance <= interactable.MaxRange)
+                {
+                    if(interactable == currentTarget)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        if(currentTarget!=null)
+                        {
+                            currentTarget.OnEndHover();
+                        }
+                        currentTarget = interactable;
+                        currentTarget.OnStartHover();
+                        return;
+                    }
+                }
+                return;
+            }
+        }
+
+        if(currentTarget!=null)
+        {
+            currentTarget.OnEndHover();
+            currentTarget=null;
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastForInteractable();
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(currentTarget!=null)
+            {
+                currentTarget.OnInteract();
+            }
+        }
+    }
+}
---
+++ Fear of the Dark/Assets/Scripts/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour, IInteractable
+{
+    public float maxRange = 100f;
+    public float MaxRange{get{return maxRange;}}
+
+    public void OnStartHover()
+    {
+        Debug.Log("ready to pick up");
+    }
+    public void OnInteract()
+    {
+        Debug.Log("interacted");
+    }
+    public void OnEndHover()
+    {
+        Debug.Log("out");
+    }
+}
