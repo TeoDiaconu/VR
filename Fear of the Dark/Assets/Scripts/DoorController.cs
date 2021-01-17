49209b25de771fc0f4fbd0967e2fce1d4baaee72
Tudor Costea <tudor@costea2008@yahoo.com>
2021-01-17 19:44:22 +0200

    doors

---
+++ Fear of the Dark/Assets/Scripts/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject door;
+    Animator _doorAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnimator = door.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _doorAnimator.SetBool("IsOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _doorAnimator.SetBool("IsOpening", false);
+    }
+}
---
+++ Fear of the Dark/Assets/Scripts/DoubleDoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorController : MonoBehaviour
+{
+    public GameObject doorA;
+    public GameObject doorB;
+    Animator _doorAnimatorA;
+    Animator _doorAnimatorB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnimatorA = doorA.GetComponent<Animator>();
+        _doorAnimatorB = doorB.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _doorAnimatorA.SetBool("IsOpening", true);
+        _doorAnimatorB.SetBool("IsOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _doorAnimatorA.SetBool("IsOpening", false);
+        _doorAnimatorB.SetBool("IsOpening", false);
+    }
+}
