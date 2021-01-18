37a23d85ebb48c1756b9c27f92236859eb03f431
Tudor Costea <tudor@costea2008@yahoo.com>
2021-01-18 14:52:24 +0200

    candles

---
+++ Fear of the Dark/Assets/Scripts/PlaceCandles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCandles : MonoBehaviour, IInteractable
+{
+    public float maxRange = 10f;
+    public float MaxRange{get{return maxRange;}}
+
+    public GameObject candles;
+    public GameObject candlesLight;
+    bool placed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candles.SetActive(placed);
+        candlesLight.SetActive(placed);
+    }
+
+    public void OnStartHover()
+    {
+        Debug.Log("Press E to place");
+    }
+    public void OnInteract()
+    {
+        if(!placed){
+            placed=true;
+            candles.SetActive(placed);
+            candlesLight.SetActive(placed);
+        }
+    }
+    public void OnEndHover()
+    {
+        Debug.Log("Candle spot out of range");
+    }
+}
