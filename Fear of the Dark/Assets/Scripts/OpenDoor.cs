92749c58bd22dd29842a74ec4cb3e02b7aa10d8d
Tudor Costea <tudor@costea2008@yahoo.com>
2021-01-18 09:49:35 +0200

    door interact

---
+++ Fear of the Dark/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour, IInteractable
+{
+    public float maxRange = 10f;
+    public float MaxRange{get{return maxRange;}}
+
+    public GameObject door;
+    Animator _doorAnimator;
+    bool isOpening = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnimator = door.GetComponent<Animator>();
+    }
+
+    public void OnStartHover()
+    {
+        Debug.Log("Press E to interact");
+    }
+    public void OnInteract()
+    {
+        isOpening=!isOpening;
+        _doorAnimator.SetBool("IsOpening", isOpening);
+    }
+    public void OnEndHover()
+    {
+        Debug.Log("Door out of range");
+    }
+}
